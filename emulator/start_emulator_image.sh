c900020bfd8b120ceaf05c1019856f62109ee657
maxwen <max.weninger@gmail.com>
2018-08-27 21:28:33 +0200

    du: switch emulator to pixel_2_xl
    Change-Id: Ife658a7d2e67ecf39c1f2f06262f4639fd582a3c

--- emulator/start_emulator_image.sh
+++ emulator/start_emulator_image.sh
@@ -3,4 +3,4 @@
 export ANDROID_BUILD_TOP=`pwd`
 export ANDROID_PRODUCT_OUT=`pwd`
 
-~/Android/Sdk/tools/emulator -verbose -skindir $ANDROID_BUILD_TOP/skins/ -skin pixel_xl -gpu host -writable-system -qemu -cpu qemu64
+~/Android/Sdk/tools/emulator -verbose -skindir $ANDROID_BUILD_TOP/skins/ -skin pixel_2_xl -gpu host -writable-system -qemu -cpu qemu64
