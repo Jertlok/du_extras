b222e6a7cb5d5eb1ee64c8460c974464bf41189f
Giuseppe Maggio <jertlokk@gmail.com>
2019-01-23 18:50:43 +0100

    du: emulator: update a bunch of stuff
    - Disable cutout;
    - Use readable DPI;
    - Fixed value to Android SDK according to my env;
    - Wipe data on every emulator launch.
    
    Change-Id: Iff12f53a82a298d6e4969e40f82e7f93fdb2b7ba

--- emulator/du.mk
+++ emulator/du.mk
@@ -10,4 +10,8 @@ PRODUCT_NAME := du_emulator
 
 DEVICE_PACKAGE_OVERLAYS += vendor/extras/emulator/overlay
 
+# Apply a reasonable DPI
+RODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=540
+
 ALLOW_MISSING_DEPENDENCIES := true
--- emulator/start_emulator_local.sh
+++ emulator/start_emulator_local.sh
@@ -2,4 +2,4 @@
 
 # assumes the image zip has been extracted in /tmp
 
-emulator -verbose -skindir $ANDROID_BUILD_TOP/vendor/extras/emulator/skins/ -skin pixel_2_xl -writable-system -memory 4096 -gpu host -wipe-data
+~/Android/Sdk/emulator/emulator -verbose -skindir $ANDROID_BUILD_TOP/vendor/extras/emulator/skins/ -skin pixel_2_xl -writable-system -gpu host -wipe-data
